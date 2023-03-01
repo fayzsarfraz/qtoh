73c24163cc7c75efc237774c65e76b1926749edc
fayzsarfraz <fayz475@gmail.com>
2023-03-01 17:40:42 +0500

    first commit

---
+++ qto/static/js/hello.js
@@ -0,0 +1,30 @@
+                     // Variable
+
+// variable can not be number
+// variable should be meaningfull
+// can not start with a number
+// variable name are case sensition
+// can not contain number or hyphen -
+// there are five type of data types in java Script
+let name = 'fayz sarfraz';  // string in quotes
+let age = 25;              // numbers 
+let isapproved = false;    // bollion
+let firstname = undefined; //   undefined
+let selectedColor = null;  // null
+console.log(name);
+
+                      //Constant
+//we can assign value to constant once 
+
+const number = '56';
+
+
+
+                    //funtion in Java Sctript
+//that is how we declare function
+function MyName(name)
+{
+    console.log('hello ' + name);
+}
+// that is how we call function
+MyName('fayz');
---
+++ search/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views  # what is the meaning of this thing when we write .
+from django.contrib import admin
+
+urlpatterns = [
+    path('', views.index,  name='search'),
+
+]
